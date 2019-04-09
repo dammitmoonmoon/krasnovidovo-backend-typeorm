4bb6d3d6b10ee22274f35471e7d5a956b4f35c5c
Yulia Modestova <jmodestova@yahoo.com>
2019-04-09 17:31:44 +0300

    RP5 scrapping with puppeteer (one test file)

---
+++ src/parser/rp5configs.ts
@@ -0,0 +1,9 @@
+export const rp5configs = {
+    tabId: '#tabSynopDLoad',
+    dateFromId: 'calender_dload',
+    dateToId: 'calender_dload2',
+    csvId: 'format2',
+    utf8Id: 'coding2',
+    createCSVSelector: '#divSynopDLoad .archButton',
+    loadCSVSelector: '#divSynopDLoad a',
+};
--- src/parser/rp5parser.ts
+++ src/parser/rp5parser.ts
@@ -1,65 +1,80 @@
 import * as puppeteer from 'puppeteer';
 import {rp5} from "./dataSources";
 import {Page} from "puppeteer";
+import {rp5configs} from "./rp5configs";
+import fetch from 'node-fetch';
+import * as fs from "fs";
+import * as path from "path";
+import * as zlib from "zlib";
+
+fs.mkdir(path.join(__dirname, 'rawData'),err => {
+    if (err) {
+        throw err;
+    }
+});
 
 const fromDate = '01.02.2019';
 const toDate = '01.04.2019';
 
-const DOWNLOAD_PATH = '/home/moonmoon/WebstormProjects/Krasnovidovo-backend-typeorm/src/csvToPostgres/rawData';
-
-puppeteer.launch({headless: false}).then(async browser => {
+puppeteer.launch().then(async browser => {
     const page = await browser.newPage();
-    await page.goto(rp5);
-    page.on('dialog', async dialog => {
-        console.log(dialog.message());
-        await dialog.dismiss();
-    });
-    await page.click('#tabSynopDLoad');
-    await setFromDate(fromDate, page);
-    await setToDate(toDate, page);
-    await setCSVFormat(page);
-    await page.click('#divSynopDLoad .archButton');
-    await page.waitFor(1000);
-    const client = await page.target().createCDPSession();
-    await client.send('Page.setDownloadBehavior', {
-        behavior: 'allow',
-        downloadPath:  DOWNLOAD_PATH
-    });
 
-    try {
-        await page.click('#divSynopDLoad a');
-    } catch (e) {
-        console.log('oops');
+    await openCSVTab(page);
+
+    await setDate(fromDate, rp5configs.dateFromId, page);
+    await setDate(toDate, rp5configs.dateToId, page);
+
+    await setCSVFormat(page);
+
+    await page.click(rp5configs.createCSVSelector);
+
+    await awaitSelector(rp5configs.loadCSVSelector, page);
+    const link = await page.evaluate((selector: string) => {
+        return (<HTMLBaseElement>document.querySelector(selector)).href;
+    }, rp5configs.loadCSVSelector);
+    const response = await fetch(link);
+    if (response.ok) {
+        const dest = fs.createWriteStream(path.join(__dirname, 'rawData', 'test.csv'));
+        response.body.pipe(zlib.createUnzip()).pipe(dest);
+    } else {
+        console.log(`Error. Response status: ${response.status}`);
     }
-    //
-    // await browser.close();
+    await browser.close();
 });
 
-const setFromDate = async (fromDate: string, page: Page) => {
-    await page.evaluate(() => {
-        const dateFromInput = document
-            .getElementById('calender_dload') as HTMLInputElement;
-        dateFromInput.value = '';
-    });
-    await page.focus('#calender_dload');
-    await page.type('#calender_dload', fromDate, {delay: 100});
+const openCSVTab = async (page: Page) => {
+    await page.goto(rp5, {waitUntil: "networkidle2"});
+    await page.click(rp5configs.tabId);
 };
 
-const setToDate = async (toDate: string, page: Page) => {
-    await page.evaluate(() => {
-        const dateFromInput = document
-            .getElementById('calender_dload2') as HTMLInputElement;
-        dateFromInput.value = '';
-    });
-    await page.focus('#calender_dload2');
-    await page.type('#calender_dload2', toDate, {delay: 100});
+const setDate = async (date: string, id: string, page: Page) => {
+    const idSelector = `#${id}`;
+    await awaitSelector(idSelector, page);
+    await removeDefaultInput(id, page);
+    await page.type(idSelector, date, {delay: 100});
 };
 
 const setCSVFormat = async (page: Page) => {
-    await page.evaluate(() => {
-        const inputFormat: HTMLInputElement = document.querySelector('#format2');
+    // for some reason, these selectors cannot be found by common Page functions
+    await page.evaluate((csvId, utf8Id) => {
+        const inputFormat: HTMLInputElement = document.querySelector(csvId);
         inputFormat.click();
-        const inputEncoding: HTMLInputElement = document.querySelector('#coding2');
+        const inputEncoding: HTMLInputElement = document.querySelector(utf8Id);
         inputEncoding.click();
-    });
+    }, `#${rp5configs.csvId}`, `#${rp5configs.utf8Id}`);
 };
+
+const awaitSelector = async (selector: string, page: Page) => {
+    try {
+        await page.waitForSelector(selector);
+    } catch (e) {
+        console.log(`Error. Selector ${selector} is unavailable`);
+    }
+};
+
+const removeDefaultInput = async (inputId: string, page: Page) => {
+    await page.evaluate((inputId) => {
+        (<HTMLInputElement>document.getElementById(inputId)).value = '';
+    }, inputId);
+};
+
